6508674373f79cc795ead40de1dafd06dc429554
ApxNord <nord-life17@yandex.ru>
2022-08-03 21:14:45 +0300

    Добавьте файлы проекта.

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using DependencyInjection.Repositories.Interfaces;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {  
+
+        private IRepository _repository;
+        //private ProductSum productSum;
+
+        public HomeController(IRepository repository 
+            /*ProductSum pSum*/)
+        {
+            _repository = repository;
+            //productSum = pSum;
+        }
+
+        public IActionResult Index([FromServices] ProductSum productSum)
+        {
+            //ViewBag.Total = productSum.Total;
+
+            ViewBag.HomeControllerGuid = _repository.ToString();
+            ViewBag.TotalGuid = productSum.Repository.ToString();
+
+            return View(_repository.Products);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,52 @@
+using DependencyInjection.Repositories;
+using DependencyInjection.Models;
+using DependencyInjection.Repositories.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddTransient<IRepository, Repository>();
+//builder.Services.AddTransient<IStorage, Storage>();
+
+IWebHostEnvironment env = builder.Environment;
+builder.Services.AddTransient<IRepository>(provider =>
+{
+    if (env.IsDevelopment())
+    {
+        var x = provider.GetService<Repository>();
+        return x;
+    }
+    else
+    {
+        return new ProductionRepository();
+    }
+});
+
+builder.Services.AddTransient<Repository>();
+
+builder.Services.AddTransient<ProductSum>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
